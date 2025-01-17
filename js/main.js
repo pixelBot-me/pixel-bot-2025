31b7e4572ac6d8ad3cf005a2bf7e6b827d1cc830
Nikita Kose <nikita@thepixelbot.com>
2025-01-17 13:03:59 +0530

    our services section done

--- js/main.js
+++ js/main.js
@@ -304,3 +304,21 @@ function toggleDropdown(element) {
 	// Prevent the default link behavior
 	event.preventDefault();
   }
+
+
+//   our services section
+const animatedEls = document.querySelectorAll("[data-animation]");
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		const animation = entry.target.getAttribute("data-animation");
+
+		if (entry.isIntersecting) {
+			entry.target.classList.add("animated", `${animation}`);
+		} else {
+			entry.target.classList.remove("animated", `${animation}`);
+		}
+	});
+});
+
+animatedEls.forEach((el) => observer.observe(el));
